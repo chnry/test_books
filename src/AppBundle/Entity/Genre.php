047ff4f970c6b409992506d45ead4980e72b2211
Roman Nikandrov <chnry@mail.ru>
2017-12-21 16:52:58 +0300

    Создана сущность Жанр

---
+++ src/AppBundle/Entity/Genre.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Genre
+ */
+class Genre
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Genre
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
